aa094c9a9983c9e2746f8f8470f0f757cc6057ed
wemhardt <142940681+wemhardt@users.noreply.github.com>
2023-11-17 10:51:28 -0500

    Update lab13will.py

--- lab13will.py
+++ lab13will.py
@@ -1,19 +1,4 @@
-import streamlit as st
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-st.title('Exploratory Data Analysis of NFL with Streamlit')
-uploaded_file = st.file_uploader("Upload your CSV file", type=["csv"])
-@st.cache_data
-def load_data(file):
-	data = pd.read_csv(file)
-	return data
-if uploaded_file is not None:
-	df = load_data(uploaded_file)
-if st.checkbox('Histogram plot of a column'):
-        column = st.selectbox('Select column for histogram', df.columns, index=2)
-        sns.histplot(df[column], bins=25)
-        st.pyplot(plt.gcf())
+
        
 
 
